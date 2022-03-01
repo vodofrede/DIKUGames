835d4bb391f1779d2259870cffacb1353e4d4f50
Frederik Palmø <frederik@palmoe.dk>
2022-03-01 12:03:57 +0100

    indsæt tom Player.cs

---
+++ Galaga/Player.cs
@@ -0,0 +1,18 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+
+namespace Galaga;
+
+public class Player {
+    private Entity entity;
+    private DynamicShape shape;
+
+    public Player(DynamicShape shape, IBaseImage image) {
+        entity = new Entity(shape, image);
+        this.shape = shape;
+    }
+
+    public void Render() {
+        // TODO: render spiller
+    }
+}
