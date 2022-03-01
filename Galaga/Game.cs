8a3fd8823600accc27c7cb90373a8b0f85726c5c
Samuel Himmelstrup <samuelhimmelstrup@gmail.com>
2022-03-01 12:28:47 +0100

    did stuff in Game class

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -3,6 +3,9 @@ using System.Numerics;
 using System.Security;
 using DIKUArcade;
 using DIKUArcade.GUI;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 using DIKUArcade.Input;
 
 namespace Galaga {
@@ -11,16 +14,16 @@ namespace Galaga {
 
         public Game(WindowArgs windowArgs) : base(windowArgs) {
             // TODO: Set key event handler (inherited window field of DIKUGame class)
-            // player = new Player(
-            //     new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
-            //     new Image(Path.Combine("Assets", "Images", "Player.png"))
-            // );
+            player = new Player(
+                new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+                new Image(Path.Combine("Assets", "Images", "Player.png"))
+            );
         }
 
         //private void KeyHandler(KeyboardAction action, KeyboardKey key) {} // TODO: Outcomment
 
         public override void Render() {
-            // this.player.Render();
+            this.player.Render();
         }
 
         public override void Update() {
